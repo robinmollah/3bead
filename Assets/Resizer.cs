b46bb8143994bd4e823cf76ac9d557463b4e15f2
Robin Sajin <Robinsajin@gmail.com>
2019-01-26 17:04:28 +0600

    [Updated] Multi screen support

---
+++ Assets/Resizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resizer : MonoBehaviour
+{
+    void Start()
+    {
+        float nScale = 3f;
+        float cWidth = Screen.width;
+        float cDpi = 416;
+        if(Screen.orientation == ScreenOrientation.Portrait)
+        {
+            nScale = (cWidth / 250f) / (cDpi / 160f);
+        }
+        transform.localScale = new Vector3(nScale, nScale, 1);
+    }
+
+    void Update()
+    {
+        
+    }
+}
